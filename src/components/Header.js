59f232591dfbb076b6009eff633f9483471b0a1b
obikakelvin <obikakelvin.2255@gmail.com>
2023-03-29 17:27:03 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+
+function Header() {
+    return (
+        <header className='header'>
+            <div className="row">
+                <div className="container">
+                    <nav className='nav'>
+                        <h4>Logo</h4>
+
+                        <ul className='nav__list'>
+                            <li>Research</li>
+                            <li>About us</li>
+                            <li>Our team</li>
+                            <li>Events</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
